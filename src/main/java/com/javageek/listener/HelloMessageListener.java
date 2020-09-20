82dbee6df1382b5d6a11d6856fc55f2c46bc69a5
Vinoth Manoharan <vinothm.manoharan@gamil.com>
2020-09-20 23:37:27 +0530

    Code for JMS to Send and receive added

--- src/main/java/com/javageek/listener/HelloMessageListener.java
+++ src/main/java/com/javageek/listener/HelloMessageListener.java
@@ -1,8 +1,13 @@
 package com.javageek.listener;
 
+import java.util.UUID;
+
+import javax.jms.JMSException;
 import javax.jms.Message;
 
+import org.springframework.jms.JmsException;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -11,11 +16,16 @@ import org.springframework.stereotype.Component;
 import com.javageek.config.JmsConfig;
 import com.javageek.model.HelloWorldMessage;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+	
+	private static int send_receive_count = 0;
+	private final JmsTemplate jmsTemplate;
 
 	@JmsListener(destination = JmsConfig.MY_QUEUE)
 	public void listen(@Payload HelloWorldMessage helloWorldMessage,
@@ -23,6 +33,23 @@ public class HelloMessageListener {
 		
 		log.info("Listener receives the message...");
 		log.info(""+helloWorldMessage);
+		
+	}
+	
+	@JmsListener(destination = JmsConfig.MY_SEND_RECEIVE_QUEUE)
+	public void listenSendReceive(@Payload HelloWorldMessage helloWorldMessage,
+						@Headers MessageHeaders headers, Message message) throws JmsException, JMSException {
+		
+		HelloWorldMessage payLoadMessage = HelloWorldMessage
+				.builder()
+				.id(UUID.randomUUID())
+				.message("Message from Listener = "+send_receive_count)
+				.build();
+		
+		log.info(""+helloWorldMessage);
+		
+		jmsTemplate.convertAndSend(message.getJMSReplyTo(), payLoadMessage);
+		log.info("Listener receives and replied...");
 		
 	}
 }
--- src/main/java/com/javageek/sender/HelloSender.java
+++ src/main/java/com/javageek/sender/HelloSender.java
@@ -2,10 +2,17 @@ package com.javageek.sender;
 
 import java.util.UUID;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javageek.config.JmsConfig;
 import com.javageek.model.HelloWorldMessage;
 
@@ -17,7 +24,11 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class HelloSender {
 	
+	private static int sent_count = 0;
+	private static int send_receive_count = 0;
+	
 	private final JmsTemplate jmsTemplate;
+	private final ObjectMapper objectMapper;
 	
 	@Scheduled(fixedRate = 2000)
 	public void sendMessage() {
@@ -26,12 +37,42 @@ public class HelloSender {
 		HelloWorldMessage message = HelloWorldMessage
 				.builder()
 				.id(UUID.randomUUID())
-				.message("Hello Vinoth!!!")
+				.message("Hello Sent only!!! "+sent_count)
 				.build();
 		
 		jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 		
 		log.info("Message Sent");
+	}
+	
+	@Scheduled(fixedRate = 2000)
+	public void sendAndReceiveMessage() throws JMSException {
+		log.info("Message Sending...");
+		
+		HelloWorldMessage message = HelloWorldMessage
+				.builder()
+				.id(UUID.randomUUID())
+				.message("Hello Send and Receive!!! "+send_receive_count)
+				.build();
+		
+		Message receivedMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_QUEUE, new MessageCreator() {
+
+			@Override
+			public Message createMessage(Session session) {
+				Message helloMessage = null;
+				try {
+					helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+					helloMessage.setStringProperty("_type", "com.javageek.model.HelloWorldMessage");
+					log.info("Sending Hello..");
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return helloMessage;
+			}
+		});
+		
+		log.info("Message Send and Received = "+receivedMessage.getBody(String.class));
 	}
 
 }
